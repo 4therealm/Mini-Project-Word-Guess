0b2c3ae21cf12284b4ba0bee8896d2fd43ddb9ce
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-02 16:22:14 -0600

    pseudo coding in progress

---
+++ kiss.js
@@ -0,0 +1,25 @@
+//play button to initiate game
+
+//empty blocks appear. number of blocks dependent on the length of the generated word. letters will either need to be hidden in blocks or generated with the blocks when the the blocks are appended. either way they need to be hidden until the player guesses that letter. then they will become visible.
+//blocks should be in grid i think, or maybe display hidden so the space is static and doesn't adjust with flex
+
+//timer interval set
+
+//to win game player must guess all the letters before timer runs out(or until choice count is exceeded?)
+//to win all letters must be guessed before timer runs out
+
+//idea: timer is visual. a llama approaching or something like that.
+
+//wins and losses displayed on screen( permanently i think, instead of at the end) 
+
+
+
+
+//key event when player presses the letter key.
+//once the key value is determined, it should be compared to the letters in the array of the generated word, for each letter does the choice === true.
+
+//when win/lose a message should be displayed abd the timer stopped.
+
+//timer should reset on startbuttopn click
+
+//wins and losses saved in local storage i think
