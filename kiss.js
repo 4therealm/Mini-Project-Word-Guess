dea4219ea78cd664d1e200e503360846a891c5ad
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-01-05 15:54:33 -0600

    somewhat working

--- kiss.js
+++ kiss.js
@@ -25,7 +25,7 @@
 //wins and losses saved in local storage i think
 
 
-//element constants
+//dynamic element constants
 
 // const word_box = document.querySelector('#word-box');
 document.addEventListener('keydown', evaluatePressedKey)
@@ -34,47 +34,100 @@ document.addEventListener('keydown', evaluatePressedKey)
 const start_button = document.querySelector('#start-button')
 start_button.addEventListener('click', startGame)
 const inputs = document.querySelector(".inputs");
-typingInput = document.querySelector(".typing-input");
-
+const typingInput = document.querySelector(".typing-input");
+const incorrect_display = document.querySelector(".incorrect")
+// managing state
 let currentWord;
+let wrongGuesses = 0;
 let incorrectLetters = [];
-let correctLetters = [];  
+let correctLetters = [];
 let pressedKey;
+let wins = 0;
+let losses = 0;
+
+// const maxGuesses = currentWord.length + 4;
+
+
+//array of words
 let word_pool = [
-{word:'succubus'},
-{word:'porcupine'},
-{word:'infinity'},
-{word:'zombie'},
-{word:'hippopotamus'},
-{word:'fibonacci'},
-{word:'daffodil'}
+  {word:'succubus'},
+  {word:'porcupine'},
+  {word:'infinity'},
+  {word:'zombie'},
+  {word:'hippopotamus'},
+  {word:'fibonacci'},
+  {word:'daffodil'}
 ];
 
+
 function startGame() {
+
   let randomObject = word_pool[Math.floor(Math.random()*word_pool.length)];
   console.log(randomObject.word)
   currentWord = randomObject.word
+  
+  loadWord(currentWord)
+
+}
+
+function loadWord(word){
+  // resetState() will remove the previously generated word and the correct/incorrect count
+  correctLetters = [];
+  incorrectLetters = [];
+  console.log(`correct letter count ${correctLetters.length} incorrect letter count ${incorrectLetters.length}`)
+
   let html = "";
-  for (let i = 0; i < currentWord.length; i++) {
+  for (let i = 0; i < word.length; i++) {
     html += `<input type="text" disabled>`
     inputs.innerHTML = html;
-    
   }
 }
+
+
+
+
+
+
 function evaluatePressedKey(e){
   pressedKey =  e.key
-console.log(pressedKey)
-if (pressedKey.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(pressedKey) && !correctLetters.includes(pressedKey)){
+// console.log(pressedKey)
+//if key pressed is a letter, and has not been pressed already
+if (pressedKey.match(/^[A-Za-z]+$/) &&
+ !incorrectLetters.includes(pressedKey) && 
+ !correctLetters.includes(pressedKey)){
+compare(pressedKey, currentWord)
+}else{wrongGuesses ++
+  console.log(`guesses = ${wrongGuesses}`)
+incorrect_display.innerText = `Incorrect Letters: ${incorrectLetters}`}
 
-  if(currentWord.includes(pressedKey)) {
-    for (let i = 0; i < currentWord.length; i++) {
-        if(currentWord[i] == pressedKey) {
-            correctLetters += pressedKey;
-            inputs.querySelectorAll("input")[i].value = pressedKey;
-        }
+
+if (correctLetters.length === currentWord.length){
+  console.log("you win")
+  wins++
+  loadWord()
+}
+}
+
+function compare(key, word) 
+{
+  if (word.includes(key))
+  {
+    for (let i = 0; i < word.length; i++)
+    {
+      if (word[i] == key) {
+          correctLetters += pressedKey;
+            
+          console.log(`correct letters :
+              ${correctLetters.length} 
+              / ${currentWord.length}`)
+          console.log(`${inputs.querySelectorAll("input")[i].value}`)
+           inputs.querySelectorAll("input")[i].value = pressedKey;
+          }
+      
     }
-} else {
-    incorrectLetters.push(pressedKey);
-}
-}
+  }else{
+          incorrectLetters.push(pressedKey);
+          console.log("Wrong letter, loser")
+          
+          }
 }
